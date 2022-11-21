1addd54a6d029f2f483cc50f2231f75174ef17f6
Debeshee <debesheedas@gmail.com>
2022-11-21 09:48:04 +0530

    Started Part 2A

--- hubconf.py
+++ hubconf.py
@@ -27,14 +27,6 @@ def get_data_circles(n_points=100):
   X, y = make_circles(n_samples=n_points, random_state=0, factor=0.8)
   return X,y
 
-def get_data_mnist():
-  pass
-  # write your code here
-  # Refer to sklearn data sets
-  X,y = None
-  # write your code ...
-  return X,y
-
 def build_kmeans(X=None,k=10):
   # k is a variable, calling function can give a different number
   # Refer to sklearn KMeans method
@@ -55,66 +47,74 @@ def compare_clusterings(ypred_1=None,ypred_2=None):
 
 ###### PART 2 ######-------------------------------------------------------------------------------------
 
-# def build_lr_model(X=None, y=None):
-#   pass
-#   lr_model = None
-#   # write your code...
-#   # Build logistic regression, refer to sklearn
-#   return lr_model
+from sklearn.datasets import load_digits
+from sklearn.linear_model import LogisticRegression
 
-# def build_rf_model(X=None, y=None):
-#   pass
-#   rf_model = None
-#   # write your code...
-#   # Build Random Forest classifier, refer to sklearn
-#   return rf_model
+def get_data_mnist():
+  # write your code here
+  # Refer to sklearn data sets
+  X,y = load_digits(return_X_y=True)
+  return X,y
 
-# def get_metrics(model=None,X=None,y=None):
-#   pass
-#   # Obtain accuracy, precision, recall, f1score, auc score - refer to sklearn metrics
-#   acc, prec, rec, f1, auc = 0,0,0,0,0
-#   # write your code here...
-#   return acc, prec, rec, f1, auc
+def build_lr_model(X=None, y=None):
+  lr_model = LogisticRegression()
+  # write your code...
+  # Build logistic regression, refer to sklearn
+  return lr_model
 
-# def get_paramgrid_lr():
-#   # you need to return parameter grid dictionary for use in grid search cv
-#   # penalty: l1 or l2
-#   lr_param_grid = None
-#   # refer to sklearn documentation on grid search and logistic regression
-#   # write your code here...
-#   return lr_param_grid
+def build_rf_model(X=None, y=None):
+  pass
+  rf_model = None
+  # write your code...
+  # Build Random Forest classifier, refer to sklearn
+  return rf_model
 
-# def get_paramgrid_rf():
-#   # you need to return parameter grid dictionary for use in grid search cv
-#   # n_estimators: 1, 10, 100
-#   # criterion: gini, entropy
-#   # maximum depth: 1, 10, None  
-#   rf_param_grid = None
-#   # refer to sklearn documentation on grid search and random forest classifier
-#   # write your code here...
-#   return rf_param_grid
+def get_metrics(model=None,X=None,y=None):
+  pass
+  # Obtain accuracy, precision, recall, f1score, auc score - refer to sklearn metrics
+  acc, prec, rec, f1, auc = 0,0,0,0,0
+  # write your code here...
+  return acc, prec, rec, f1, auc
 
-# def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
+def get_paramgrid_lr():
+  # you need to return parameter grid dictionary for use in grid search cv
+  # penalty: l1 or l2
+  lr_param_grid = None
+  # refer to sklearn documentation on grid search and logistic regression
+  # write your code here...
+  return lr_param_grid
+
+def get_paramgrid_rf():
+  # you need to return parameter grid dictionary for use in grid search cv
+  # n_estimators: 1, 10, 100
+  # criterion: gini, entropy
+  # maximum depth: 1, 10, None  
+  rf_param_grid = None
+  # refer to sklearn documentation on grid search and random forest classifier
+  # write your code here...
+  return rf_param_grid
+
+def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
   
-#   # you need to invoke sklearn grid search cv function
-#   # refer to sklearn documentation
-#   # the cv parameter can change, ie number of folds  
+  # you need to invoke sklearn grid search cv function
+  # refer to sklearn documentation
+  # the cv parameter can change, ie number of folds  
   
-#   # metrics = [] the evaluation program can change what metrics to choose
+  # metrics = [] the evaluation program can change what metrics to choose
   
-#   grid_search_cv = None
-#   # create a grid search cv object
-#   # fit the object on X and y input above
-#   # write your code here...
+  grid_search_cv = None
+  # create a grid search cv object
+  # fit the object on X and y input above
+  # write your code here...
   
-#   # metric of choice will be asked here, refer to the-scoring-parameter-defining-model-evaluation-rules of sklearn documentation
+  # metric of choice will be asked here, refer to the-scoring-parameter-defining-model-evaluation-rules of sklearn documentation
   
-#   # refer to cv_results_ dictonary
-#   # return top 1 score for each of the metrics given, in the order given in metrics=... list
+  # refer to cv_results_ dictonary
+  # return top 1 score for each of the metrics given, in the order given in metrics=... list
   
-#   top1_scores = []
+  top1_scores = []
   
-#   return top1_scores
+  return top1_scores
 
 # ###### PART 3 ######-------------------------------------------------------------------
 
